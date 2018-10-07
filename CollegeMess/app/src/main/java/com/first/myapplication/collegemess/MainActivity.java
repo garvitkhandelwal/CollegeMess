b28a95f45aed5bb76f110553e061f7a352c871b7
Garvit Khandelwal <garvitkhandelwal14@gmail.com>
2018-10-07 13:30:29 +0530

    Initial commit

---
+++ CollegeMess/app/src/main/java/com/first/myapplication/collegemess/MainActivity.java
@@ -0,0 +1,20 @@
+package com.first.myapplication.collegemess;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v){
+        Intent i = new Intent(this,loginActivity.class);
+        startActivity(i);
+    }
+}
